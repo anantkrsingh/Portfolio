29a37e3c7514b3518e3b7484cb88fdccf14e8fac
anantkrsingh <anantkrplay@gmail.com>
2024-12-04 12:58:06 +0530

    Updated the card href

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -19,8 +19,8 @@ const RecentProjects = () => {
             key={item.id}
           >
             <PinContainer
-              title="/ui.aceternity.com"
-              href="https://twitter.com/mannupaaji"
+              title={`${item.title.slice(0,9)}...`}
+              href={item.link}
             >
               <div className="relative flex items-center justify-center sm:w-[570px] w-[80vw] overflow-hidden  h-[30vh] lg:h-[30vh] mb-10">
                 <div
